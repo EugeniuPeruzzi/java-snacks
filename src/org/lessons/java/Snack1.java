0bf2123da79d42def10caba3fde0d8fb655ba8a8
EugeniuPeruzzi <jaka1997@live.it>
2023-11-10 11:21:13 +0100

    snack 1 done

--- src/org/lessons/java/Snack1.java
+++ src/org/lessons/java/Snack1.java
@@ -1,9 +1,25 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class Snack1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.print("Inserisci un numero");
+		//call out scanner
+		Scanner in = new Scanner(System.in);
+		int inputNumber = in.nextInt();
+		
+		if (inputNumber %2 != 0) {
+			inputNumber ++;
+			System.out.println(inputNumber);
+		}
+		else {
+			System.out.println(inputNumber);
+		}
+		//colse
+		in.close();
 
 	}
 
